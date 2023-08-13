46c81aee9686d0dde94ae195305451c80209b6d2
cwatts2018 <142118626+cwatts2018@users.noreply.github.com>
2023-08-12 20:00:09 -0700

    Update gradientDescentMulti.m

--- ex1/gradientDescentMulti.m
+++ ex1/gradientDescentMulti.m
@@ -7,11 +7,11 @@ function [theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters
 m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
 
-for iter = 1:num_iters
-    theta = theta - alpha/m * X' *(X * theta - y);
-    % Save the cost J in every iteration    
-    J_history(iter) = computeCostMulti(X, y, theta);
-
-end
+    for iter = 1:num_iters
+        theta = theta - alpha/m * X' *(X * theta - y);
+        % Save the cost J in every iteration    
+        J_history(iter) = computeCostMulti(X, y, theta);
+    
+    end
 
 end
