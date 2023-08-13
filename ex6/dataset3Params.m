6f08841e7a1f1c544f28a788cdc61ed71cca953f
cwatts2018 <142118626+cwatts2018@users.noreply.github.com>
2023-08-13 00:16:59 -0700

    Update dataset3Params.m

--- ex6/dataset3Params.m
+++ ex6/dataset3Params.m
@@ -3,48 +3,35 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 %where you select the optimal (C, sigma) learning parameters to use for SVM
 %with RBF kernel
 %   [C, sigma] = DATASET3PARAMS(X, y, Xval, yval) returns your choice of C and 
-%   sigma. You should complete this function to return the optimal C and 
-%   sigma based on a cross-validation set.
+%   sigma. 
 %
 
-% You need to return the following variables correctly.
-C = 1;
-sigma = 0.3;
-
-% ====================== YOUR CODE HERE ======================
-% Instructions: Fill in this function to return the optimal C and sigma
-%               learning parameters found using the cross validation set.
-%               You can use svmPredict to predict the labels on the cross
-%               validation set. For example, 
-%                   predictions = svmPredict(model, Xval);
-%               will return the predictions on the cross validation set.
-%
-%  Note: You can compute the prediction error using 
-%        mean(double(predictions ~= yval))
-%
-C_options = [0.01; 0.03; 0.1; 0.3; 1; 3; 10; 30];
-sig_options = [0.01; 0.03; 0.1; 0.3; 1; 3; 10; 30];
-%er = zeros(8,8); %C by sigma
-
-model = svmTrain(X, y, C_options(1), @(x1, x2) gaussianKernel(x1, x2, sigma(1)));
-predictions = svmPredict(model, Xval);
-prevEr = mean(double(predictions ~= yval));
-er = mean(double(predictions ~= yval));
-C = 0.01;
-sigma = 0.01;
-
-for cur_c = 1:length(C_options)
-  for cur_sig = 1:length(sig_options)
-    model = svmTrain(X, y, C_options(cur_c), @(x1, x2) gaussianKernel(x1, x2, sig_options(cur_sig)));
-    predictions = svmPredict(model, Xval);
-    if mean(double(predictions ~= yval)) < er
-      er = mean(double(predictions ~= yval));
-      C = C_options(cur_c);
-      sigma = sig_options(cur_sig);
-    %endif
-    %Computes fraction that are incorrect
-    %er(cur_c, cur_sig) = mean(double(predictions ~= yval));
-  end;
+  C = 1;
+  sigma = 0.3;
+  
+  C_options = [0.01; 0.03; 0.1; 0.3; 1; 3; 10; 30];
+  sig_options = [0.01; 0.03; 0.1; 0.3; 1; 3; 10; 30];
+  %er = zeros(8,8); %C by sigma
+  
+  model = svmTrain(X, y, C_options(1), @(x1, x2) gaussianKernel(x1, x2, sigma(1)));
+  predictions = svmPredict(model, Xval);
+  prevEr = mean(double(predictions ~= yval));
+  er = mean(double(predictions ~= yval));
+  C = 0.01;
+  sigma = 0.01;
+  
+  for cur_c = 1:length(C_options)
+    for cur_sig = 1:length(sig_options)
+      model = svmTrain(X, y, C_options(cur_c), @(x1, x2) gaussianKernel(x1, x2, sig_options(cur_sig)));
+      predictions = svmPredict(model, Xval);
+      if mean(double(predictions ~= yval)) < er
+        er = mean(double(predictions ~= yval));
+        C = C_options(cur_c);
+        sigma = sig_options(cur_sig);
+      %endif
+      %Computes fraction that are incorrect
+      %er(cur_c, cur_sig) = mean(double(predictions ~= yval));
+    end;
 end;
 
 
