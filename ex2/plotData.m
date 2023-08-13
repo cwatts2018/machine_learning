7a8825af98191848a012b55a019da912baf72b74
cwatts2018 <142118626+cwatts2018@users.noreply.github.com>
2023-08-12 20:33:59 -0700

    Update plotData.m

--- ex2/plotData.m
+++ ex2/plotData.m
@@ -3,32 +3,11 @@ function plotData(X, y)
 %   PLOTDATA(x,y) plots the data points with + for the positive examples
 %   and o for the negative examples. X is assumed to be a Mx2 matrix.
 
-% Create New Figure
-figure; hold on;
-neg = find(y==0); pos = find(y==1);
-plot(X(pos,1),X(pos,2), 'k+', 'LineWidth', 2, 'Markersize', 7);
-plot(X(neg,1),X(neg,2), 'ko', 'LineWidth', 2, 'Markersize', 7);
-
-
-
-% ====================== YOUR CODE HERE ======================
-% Instructions: Plot the positive and negative examples on a
-%               2D plot, using the option 'k+' for the positive
-%               examples and 'ko' for the negative examples.
-%
-
-
-
-
-
-
-
-
-
-% =========================================================================
-
-
-
-hold off;
+  % Create New Figure
+  figure; hold on;
+  neg = find(y==0); pos = find(y==1);
+  plot(X(pos,1),X(pos,2), 'k+', 'LineWidth', 2, 'Markersize', 7);
+  plot(X(neg,1),X(neg,2), 'ko', 'LineWidth', 2, 'Markersize', 7);
+  hold off;
 
 end
