64618c86792bbe03d39803632419fbe16c667f52
cwatts2018 <142118626+cwatts2018@users.noreply.github.com>
2023-08-12 20:06:31 -0700

    Update gradientDescent.m

--- ex1/gradientDescent.m
+++ ex1/gradientDescent.m
@@ -3,15 +3,15 @@ function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
 %   theta = GRADIENTDESCENT(X, y, theta, alpha, num_iters) updates theta by 
 %   taking num_iters gradient steps with learning rate alpha
 
-% Initialize some useful values
-m = length(y); % number of training examples
-J_history = zeros(num_iters, 1);
-
-    for iter = 1:num_iters
-        theta = theta - alpha/m * X' *(X * theta - y);
-        % Save the cost J in every iteration    
-        J_history(iter) = computeCost(X, y, theta);
+    % Initialize some useful values
+    m = length(y); % number of training examples
+    J_history = zeros(num_iters, 1);
     
-    end
+        for iter = 1:num_iters
+            theta = theta - alpha/m * X' *(X * theta - y);
+            % Save the cost J in every iteration    
+            J_history(iter) = computeCost(X, y, theta);
+        
+        end
 
 end
