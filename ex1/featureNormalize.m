49c96f0158062ee75a05168502b805f0c324014a
cwatts2018 <142118626+cwatts2018@users.noreply.github.com>
2023-08-12 20:06:15 -0700

    Update featureNormalize.m

--- ex1/featureNormalize.m
+++ ex1/featureNormalize.m
@@ -5,15 +5,15 @@ function [X_norm, mu, sigma] = featureNormalize(X)
 %   is 1. This is a good preprocessing step to do when
 %   working with learning algorithms.
 
-% Set values
-X_norm = X;
-mu = zeros(1, size(X, 2)); %the 2 refers to the # of rows
-sigma = zeros(1, size(X, 2));
-
-mu = mean(X);
-sigma = std(X);
-[r,c] = size(X);
-for i = 1:c  
-  X_norm(:,i) =(X(:,i)- mu(1,i))/sigma(1,i);
-  end 
+  % Set values
+  X_norm = X;
+  mu = zeros(1, size(X, 2)); %the 2 refers to the # of rows
+  sigma = zeros(1, size(X, 2));
+  
+  mu = mean(X);
+  sigma = std(X);
+  [r,c] = size(X);
+  for i = 1:c  
+    X_norm(:,i) =(X(:,i)- mu(1,i))/sigma(1,i);
+    end 
 end
