4e6c91c666476adc3f29b36339601ff445724ad0
cwatts2018 <142118626+cwatts2018@users.noreply.github.com>
2023-08-13 00:57:51 -0700

    Update normalizeRatings.m

--- ex8/normalizeRatings.m
+++ ex8/normalizeRatings.m
@@ -5,13 +5,13 @@ function [Ynorm, Ymean] = normalizeRatings(Y, R)
 %   has a rating of 0 on average, and returns the mean rating in Ymean.
 %
 
-[m, n] = size(Y);
-Ymean = zeros(m, 1);
-Ynorm = zeros(size(Y));
-for i = 1:m
-    idx = find(R(i, :) == 1);
-    Ymean(i) = mean(Y(i, idx));
-    Ynorm(i, idx) = Y(i, idx) - Ymean(i);
-end
+    [m, n] = size(Y);
+    Ymean = zeros(m, 1);
+    Ynorm = zeros(size(Y));
+    for i = 1:m
+        idx = find(R(i, :) == 1);
+        Ymean(i) = mean(Y(i, idx));
+        Ynorm(i, idx) = Y(i, idx) - Ymean(i);
+    end
 
 end
