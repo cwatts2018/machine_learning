cf8194f705448c9eed105d0d1c26df284b9e50e1
cwatts2018 <142118626+cwatts2018@users.noreply.github.com>
2023-08-13 00:14:18 -0700

    Update trainLinearReg.m

--- ex5/trainLinearReg.m
+++ ex5/trainLinearReg.m
@@ -6,16 +6,16 @@ function [theta] = trainLinearReg(X, y, lambda)
 %   trained parameters theta.
 %
 
-% Initialize Theta
-initial_theta = zeros(size(X, 2), 1); 
-
-% Create "short hand" for the cost function to be minimized
-costFunction = @(t) linearRegCostFunction(X, y, t, lambda);
-
-% Now, costFunction is a function that takes in only one argument
-options = optimset('MaxIter', 200, 'GradObj', 'on');
-
-% Minimize using fmincg
-theta = fmincg(costFunction, initial_theta, options);
+  % Initialize Theta
+  initial_theta = zeros(size(X, 2), 1); 
+  
+  % Create "short hand" for the cost function to be minimized
+  costFunction = @(t) linearRegCostFunction(X, y, t, lambda);
+  
+  % Now, costFunction is a function that takes in only one argument
+  options = optimset('MaxIter', 200, 'GradObj', 'on');
+  
+  % Minimize using fmincg
+  theta = fmincg(costFunction, initial_theta, options);
 
 end
