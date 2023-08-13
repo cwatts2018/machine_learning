08adeaf5da6dd65263269b6769affc16543d5c28
cwatts2018 <142118626+cwatts2018@users.noreply.github.com>
2023-08-13 00:25:34 -0700

    Update readFile.m

--- ex6/readFile.m
+++ ex6/readFile.m
@@ -4,15 +4,15 @@ function file_contents = readFile(filename)
 %   contents in file_contents
 %
 
-% Load File
-fid = fopen(filename);
-if fid
-    file_contents = fscanf(fid, '%c', inf);
-    fclose(fid);
-else
-    file_contents = '';
-    fprintf('Unable to open %s\n', filename);
-end
+    % Load File
+    fid = fopen(filename);
+    if fid
+        file_contents = fscanf(fid, '%c', inf);
+        fclose(fid);
+    else
+        file_contents = '';
+        fprintf('Unable to open %s\n', filename);
+    end
 
 end
 
