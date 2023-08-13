391a86195bdfb02832c16afd0265b87385001620
cwatts2018 <142118626+cwatts2018@users.noreply.github.com>
2023-08-12 19:56:19 -0700

    Update ex1.m

--- ex1/ex1.m
+++ ex1/ex1.m
@@ -1,33 +1,7 @@
-%% Machine Learning Online Class - Exercise 1: Linear Regression
-
-%  Instructions
-%  ------------
-%
-%  This file contains code that helps you get started on the
-%  linear exercise. You will need to complete the following functions
-%  in this exericse:
-%
-%     warmUpExercise.m
-%     plotData.m
-%     gradientDescent.m
-%     computeCost.m
-%     gradientDescentMulti.m
-%     computeCostMulti.m
-%     featureNormalize.m
-%     normalEqn.m
-%
-%  For this exercise, you will not need to change any code in this file,
-%  or any other files other than those mentioned above.
-%
-% x refers to the population size in 10,000s
-% y refers to the profit in $10,000s
-%
-
 %% Initialization
 clear ; close all; clc
 
 %% ==================== Part 1: Basic Function ====================
-% Complete warmUpExercise.m
 fprintf('Running warmUpExercise ... \n');
 fprintf('5x5 Identity Matrix: \n');
 warmUpExercise()
@@ -43,7 +17,6 @@ X = data(:, 1); y = data(:, 2);
 m = length(y); % number of training examples
 
 % Plot Data
-% Note: You have to complete the code in plotData.m
 plotData(X, y);
 
 fprintf('Program paused. Press enter to continue.\n');
@@ -116,7 +89,6 @@ for i = 1:length(theta0_vals)
 	  J_vals(i,j) = computeCost(X, y, t);
     end
 end
-
 
 % Because of the way meshgrids work in the surf command, we need to
 % transpose J_vals before calling surf, or else the axes will be flipped
