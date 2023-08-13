fee13be20981b56a155b44808249072dfd91a5dc
cwatts2018 <142118626+cwatts2018@users.noreply.github.com>
2023-08-13 00:15:09 -0700

    Update validationCurve.m

--- ex5/validationCurve.m
+++ ex5/validationCurve.m
@@ -9,80 +9,49 @@ function [lambda_vec, error_train, error_val] = ...
 %       y) and validation set (Xval, yval).
 %
 
-% Selected values of lambda (you should not change this)
-lambda_vec = [0 0.001 0.003 0.01 0.03 0.1 0.3 1 3 10]';
-
-% You need to return these variables correctly.
-error_train = zeros(length(lambda_vec), 1);
-error_val = zeros(length(lambda_vec), 1);
-
-% ====================== YOUR CODE HERE ======================
-% Instructions: Fill in this function to return training errors in 
-%               error_train and the validation errors in error_val. The 
-%               vector lambda_vec contains the different lambda parameters 
-%               to use for each calculation of the errors, i.e, 
-%               error_train(i), and error_val(i) should give 
-%               you the errors obtained after training with 
-%               lambda = lambda_vec(i)
-%
-% Note: You can loop over lambda_vec with the following:
-%
-%       for i = 1:length(lambda_vec)
-%           lambda = lambda_vec(i);
-%           % Compute train / val errors when training linear 
-%           % regression with regularization parameter lambda
-%           % You should store the result in error_train(i)
-%           % and error_val(i)
-%           ....
-%           
-%       end
-%
-%
-for i = 1:length(lambda_vec)
-  theta = trainLinearReg(X,y,lambda_vec(i,:));
-  [J, grad] = linearRegCostFunction(X, y, theta, 0);
-  [J1, grad1] = linearRegCostFunction(Xval, yval, theta, 0);
-  
-  error_train(i,:) = J;
-  error_val(i,:) = J1;
-end;
-
-%prints min error_val and its index
-%[a,b] = min(error_val);
-%a
-%b
-
-%Taking average errors
-%for r = 1:50
-%  for i = 1:size(X,1)-5
-%    index = rand(1)*size(X,1);
-%    newX = [newX; X(index)];
-%    newY = [newY; y(index)];
-%  end;
-  
-  %for i = 1:size(Xval,1)-5
-%  index = rand(1)*size(Xval,1);
-%    newXval = [newXval; Xval(index)];
-%    newyval = [newyval; yval(index)];
-%  end;
-
-
-%  for i = 1:length(lambda_vec)
-%  theta = trainLinearReg(newX,newy,lambda_vec(i,:));
-%  [J, grad] = linearRegCostFunction(newX, newy, theta, 0);
-%  [J1, grad1] = linearRegCostFunction(newXval, newyval, theta, 0);
-  
-%  error_train(i,:) = (error_train(i,:) + J)/2;
-%  error_val(i,:) = (error_val(i,:) + J)/2;
-%  end;
-%endfor
-
-
-
-
-
-
-
-% =========================================================================
-
+    % Selected values of lambda
+    lambda_vec = [0 0.001 0.003 0.01 0.03 0.1 0.3 1 3 10]';
+    
+    error_train = zeros(length(lambda_vec), 1);
+    error_val = zeros(length(lambda_vec), 1);
+    
+    for i = 1:length(lambda_vec)
+      theta = trainLinearReg(X,y,lambda_vec(i,:));
+      [J, grad] = linearRegCostFunction(X, y, theta, 0);
+      [J1, grad1] = linearRegCostFunction(Xval, yval, theta, 0);
+      
+      error_train(i,:) = J;
+      error_val(i,:) = J1;
+    end;
+    
+    %prints min error_val and its index
+    %[a,b] = min(error_val);
+    %a
+    %b
+    
+    %Taking average errors
+    %for r = 1:50
+    %  for i = 1:size(X,1)-5
+    %    index = rand(1)*size(X,1);
+    %    newX = [newX; X(index)];
+    %    newY = [newY; y(index)];
+    %  end;
+      
+      %for i = 1:size(Xval,1)-5
+    %  index = rand(1)*size(Xval,1);
+    %    newXval = [newXval; Xval(index)];
+    %    newyval = [newyval; yval(index)];
+    %  end;
+    
+    
+    %  for i = 1:length(lambda_vec)
+    %  theta = trainLinearReg(newX,newy,lambda_vec(i,:));
+    %  [J, grad] = linearRegCostFunction(newX, newy, theta, 0);
+    %  [J1, grad1] = linearRegCostFunction(newXval, newyval, theta, 0);
+      
+    %  error_train(i,:) = (error_train(i,:) + J)/2;
+    %  error_val(i,:) = (error_val(i,:) + J)/2;
+    %  end;
+    %endfor
+    
 end
