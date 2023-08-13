1447a8417d8d329704a9e1ffccee63ed2a81d845
cwatts2018 <142118626+cwatts2018@users.noreply.github.com>
2023-08-13 00:55:00 -0700

    Update ex8_cofi.m

--- ex8/ex8_cofi.m
+++ ex8/ex8_cofi.m
@@ -1,24 +1,4 @@
-%% Machine Learning Online Class
-%  Exercise 8 | Anomaly Detection and Collaborative Filtering
-%
-%  Instructions
-%  ------------
-%
-%  This file contains code that helps you get started on the
-%  exercise. You will need to complete the following functions:
-%
-%     estimateGaussian.m
-%     selectThreshold.m
-%     cofiCostFunc.m
-%
-%  For this exercise, you will not need to change any code in this file,
-%  or any other files other than those mentioned above.
-%
-
 %% =============== Part 1: Loading movie ratings dataset ================
-%  You will start by loading the movie ratings dataset to understand the
-%  structure of the data.
-%  
 fprintf('Loading movie ratings dataset.\n\n');
 
 %  Load data
@@ -30,11 +10,11 @@ load ('ex8_movies.mat');
 %  R is a 1682x943 matrix, where R(i,j) = 1 if and only if user j gave a
 %  rating to movie i
 
-%  From the matrix, we can compute statistics like average rating.
+%  From the matrix, can compute statistics like average rating.
 fprintf('Average rating for movie 1 (Toy Story): %f / 5\n\n', ...
         mean(Y(1, R(1, :))));
 
-%  We can "visualize" the ratings matrix by plotting it with imagesc
+%  "visualize" the ratings matrix by plotting it with imagesc
 imagesc(Y);
 ylabel('Movies');
 xlabel('Users');
@@ -43,11 +23,6 @@ fprintf('\nProgram paused. Press enter to continue.\n');
 pause;
 
 %% ============ Part 2: Collaborative Filtering Cost Function ===========
-%  You will now implement the cost function for collaborative filtering.
-%  To help you debug your cost function, we have included set of weights
-%  that we trained on that. Specifically, you should complete the code in 
-%  cofiCostFunc.m to return J.
-
 %  Load pre-trained weights (X, Theta, num_users, num_movies, num_features)
 load ('ex8_movieParams.mat');
 
@@ -70,10 +45,6 @@ pause;
 
 
 %% ============== Part 3: Collaborative Filtering Gradient ==============
-%  Once your cost function matches up with ours, you should now implement 
-%  the collaborative filtering gradient function. Specifically, you should 
-%  complete the code in cofiCostFunc.m to return the grad argument.
-%  
 fprintf('\nChecking Gradients (without regularization) ... \n');
 
 %  Check gradients by running checkNNGradients
@@ -84,11 +55,6 @@ pause;
 
 
 %% ========= Part 4: Collaborative Filtering Cost Regularization ========
-%  Now, you should implement regularization for the cost function for 
-%  collaborative filtering. You can implement it by adding the cost of
-%  regularization to the original cost computation.
-%  
-
 %  Evaluate cost function
 J = cofiCostFunc([X(:) ; Theta(:)], Y, R, num_users, num_movies, ...
                num_features, 1.5);
@@ -100,12 +66,7 @@ fprintf('\nProgram paused. Press enter to continue.\n');
 pause;
 
 
-%% ======= Part 5: Collaborative Filtering Gradient Regularization ======
-%  Once your cost matches up with ours, you should proceed to implement 
-%  regularization for the gradient. 
-%
-
-%  
+%% ======= Part 5: Collaborative Filtering Gradient Regularization ====== 
 fprintf('\nChecking Gradients (with regularization) ... \n');
 
 %  Check gradients by running checkNNGradients
@@ -116,25 +77,20 @@ pause;
 
 
 %% ============== Part 6: Entering ratings for a new user ===============
-%  Before we will train the collaborative filtering model, we will first
-%  add ratings that correspond to a new user that we just observed. This
-%  part of the code will also allow you to put in your own ratings for the
-%  movies in our dataset!
-%
 movieList = loadMovieList();
 
 %  Initialize my ratings
 my_ratings = zeros(1682, 1);
 
 % Check the file movie_idx.txt for id of each movie in our dataset
-% For example, Toy Story (1995) has ID 1, so to rate it "4", you can set
+% For example, Toy Story (1995) has ID 1, so to rate it "4", set
 my_ratings(1) = 4;
 
-% Or suppose did not enjoy Silence of the Lambs (1991), you can set
+% Or suppose did not enjoy Silence of the Lambs (1991), set
 my_ratings(98) = 2;
 
-% We have selected a few movies we liked / did not like and the ratings we
-% gave are as follows:
+% Movies liked / not liked and the ratings 
+% given are as follows:
 my_ratings(7) = 3;
 my_ratings(12)= 5;
 my_ratings(54) = 4;
@@ -158,10 +114,6 @@ pause;
 
 
 %% ================== Part 7: Learning Movie Ratings ====================
-%  Now, you will train the collaborative filtering model on a movie rating 
-%  dataset of 1682 movies and 943 users
-%
-
 fprintf('\nTraining collaborative filtering...\n');
 
 %  Load data
@@ -211,10 +163,6 @@ fprintf('\nProgram paused. Press enter to continue.\n');
 pause;
 
 %% ================== Part 8: Recommendation for you ====================
-%  After training the model, you can now make recommendations by computing
-%  the predictions matrix.
-%
-
 p = X * Theta';
 my_predictions = p(:,1) + Ymean;
 
